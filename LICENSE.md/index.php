9da8c9c2a517957542d8966a2dcdd59ffd0d9738
Bludova <32148601+Bludova@users.noreply.github.com>
2017-11-14 12:52:20 +0300

    Update index.php

--- LICENSE.md/index.php
+++ LICENSE.md/index.php
@@ -10,3 +10,14 @@
   </select>
   <input type='submit' name='assign' value='Переложить ответственность'>
   </form>
+
+
+
+
+// в Нетологии если просмотреть код к этому заданию форма реализована так:
+
+<form method='POST'>  
+  <select name='assigned_user_id'>
+    <option value='user_5_task_423'>slava</option>
+  </select>
+</form>
